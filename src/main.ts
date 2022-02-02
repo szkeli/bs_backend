7194fc2023d528e82b23565330915716779dd7b9
root <root@DESKTOP-Q664A32.localdomain>
2022-02-02 09:10:31 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap () {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter())
-  app.enableCors()
   await app.listen(process.env.PORT, '0.0.0.0')
 }
 
