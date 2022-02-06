185763de60ea4852ebbd399a8cfa11fb339f4a8b
root <root@DESKTOP-Q664A32.localdomain>
2022-02-06 20:05:41 +0800

    update: 限制使用删库函数的身份

--- src/db/db.resolver.ts
+++ src/db/db.resolver.ts
@@ -1,15 +1,15 @@
+import { ForbiddenException } from '@nestjs/common'
 import { Mutation, Resolver } from '@nestjs/graphql'
 
-import { Roles } from '../auth/decorator'
+import { Admin } from '../admin/models/admin.model'
+import { CurrentUser, Roles } from '../auth/decorator'
 import { Role } from '../auth/model/auth.model'
 import { DbService } from './db.service'
 import { SetDbSchema } from './model/db.model'
 
 @Resolver()
 export class DbResolver {
-  constructor (private readonly dbService: DbService) {
-
-  }
+  constructor (private readonly dbService: DbService) {}
 
   @Mutation(() => SetDbSchema)
   @Roles(Role.Admin)
@@ -18,12 +18,16 @@ export class DbResolver {
   }
 
   @Mutation(() => Boolean)
-  async dropAllData () {
+  @Roles(Role.Admin)
+  async dropAllData (@CurrentUser() admin: Admin) {
+    if (admin.userId !== 'system') { throw new ForbiddenException('请使用system账号删库') }
     return await this.dbService.dropAll()
   }
 
   @Mutation(() => Boolean)
-  async dropData () {
+  @Roles(Role.Admin)
+  async dropData (@CurrentUser() admin: Admin) {
+    if (admin.userId !== 'system') { throw new ForbiddenException('请使用system账号删库') }
     return await this.dbService.dropData()
   }
 }
