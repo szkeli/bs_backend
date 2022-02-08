44aba75b7f9ac411b590f8c5672c39450fe75490
root <root@DESKTOP-Q664A32.localdomain>
2022-02-08 22:27:11 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
-import { readFileSync } from 'fs'
 
 import { AppModule } from './app.module'
-
-const httpsOptions = {
-  key: readFileSync('./keys/api.szlikeyou.com.key'),
-  cert: readFileSync('./keys/api.szlikeyou.com.crt')
-}
 
 async function bootstrap () {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({
-      https: httpsOptions
-    }))
+    new FastifyAdapter())
   await app.listen(process.env.PORT, '0.0.0.0')
 }
 
