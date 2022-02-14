7257658071863f5c366b8145f43f5bc2079dafc9
root <root@DESKTOP-Q664A32.localdomain>
2022-02-14 17:13:27 +0800

    update: 支持查看个人通知

--- src/notifications/notifications.resolver.ts
+++ src/notifications/notifications.resolver.ts
@@ -1,9 +1,26 @@
-import { Resolver } from '@nestjs/graphql'
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql'
 
+import { PostAndCommentUnion } from '../deletes/models/deletes.model'
+import { User } from '../user/models/user.model'
 import { Notification } from './models/notifications.model'
 import { NotificationsService } from './notifications.service'
 
 @Resolver(of => Notification)
 export class NotificationsResolver {
   constructor (private readonly notificationsService: NotificationsService) {}
+
+  @ResolveField(of => PostAndCommentUnion, { description: '通知涉及的对象' })
+  async about (@Parent() notification: Notification) {
+    return await this.notificationsService.about(notification.id)
+  }
+
+  @ResolveField(of => User, { description: '被通知的对象' })
+  async to (@Parent() notification: Notification) {
+    return await this.notificationsService.to(notification.id)
+  }
+
+  @ResolveField(of => User, { description: '通知的创建者，匿名评论时为空', nullable: true })
+  async creator (@Parent() notification: Notification) {
+    return await this.notificationsService.creator(notification.id)
+  }
 }
--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,14 +1,73 @@
 import { Injectable } from '@nestjs/common'
 
+import { Comment } from '../comment/models/comment.model'
 import { ORDER_BY } from '../connections/models/connections.model'
 import { DbService } from '../db/db.service'
-import { RelayPagingConfigArgs } from '../posts/models/post.model'
+import { PostAndCommentUnion } from '../deletes/models/deletes.model'
+import { Post, RelayPagingConfigArgs } from '../posts/models/post.model'
 import { btoa, relayfyArrayForward } from '../tool'
+import { User } from '../user/models/user.model'
 import { Notification, NotificationsConnection } from './models/notifications.model'
 
 @Injectable()
 export class NotificationsService {
+  async to (id: string) {
+    const query = `
+        query v($notificationId: string) {
+            var(func: uid($notificationId)) @filter(type(Notification)) {
+                to as to @filter(type(User))
+            }
+            to(func: uid(to)) {
+                id: uid
+                expand(_all_)
+            }
+        }
+      `
+    const res = await this.dbService.commitQuery<{to: User[]}>({ query, vars: { $notificationId: id } })
+    return res.to[0]
+  }
+
+  async creator (id: string) {
+    const query = `
+    query v($notificationId: string) {
+        var(func: uid($notificationId)) @filter(type(Notification)) {
+            creator as creator @filter(type(User))
+        }
+        creator(func: uid(creator)) {
+            id: uid
+            expand(_all_)
+        }
+    }
+  `
+    const res = await this.dbService.commitQuery<{creator: User[]}>({ query, vars: { $notificationId: id } })
+    return res.creator[0]
+  }
+
   constructor (private readonly dbService: DbService) {}
+
+  async about (id: string) {
+    const query = `
+        query v($notificationId: string) {
+            var(func: uid($notificationId)) @filter(type(Notification)) {
+                about as about @filter(type(Post) or type(Comment))
+            }
+            about(func: uid(about)) {
+                id: uid
+                expand(_all_)
+                dgraph.type
+            }
+        }
+      `
+    const res = await this.dbService.commitQuery<{about: Array<(typeof PostAndCommentUnion) & {'dgraph.type': string[]}>}>({ query, vars: { $notificationId: id } })
+    const about = res.about[0]
+
+    if (about?.['dgraph.type']?.includes('Post')) {
+      return new Post(about as unknown as Post)
+    }
+    if (about?.['dgraph.type']?.includes('Comment')) {
+      return new Comment(about as unknown as Comment)
+    }
+  }
 
   async findNotificationsByXid (id: string, { orderBy, first, last, after, before }: RelayPagingConfigArgs) {
     after = btoa(after)
