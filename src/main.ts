5c33b1ddf1f36e7aeade1a9b7a453f2b19e54824
famouscat <3142362556@qq.com>
2022-05-25 21:17:53 +0800

    update: 取消使用 log4js 作为日志输出

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,15 @@
 import { NestFactory } from '@nestjs/core'
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
-import { getLogger } from 'xmcommon'
 
 import { AppModule } from './app.module'
-import { NestLogger } from './log4js/nest.logger'
 
-const log = getLogger(__filename)
-log.info('app running...')
 async function bootstrap () {
   process.setMaxListeners(500)
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
-    {
-      logger: new NestLogger()
-    }
+    new FastifyAdapter()
   )
   await app.listen(process.env.PORT, '0.0.0.0')
-  log.info(`running on ${process.env.PORT}`)
 }
 
 bootstrap().catch(e => { throw e })
