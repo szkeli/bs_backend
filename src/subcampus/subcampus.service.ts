b4cecff385a323bd1f0a9358d00c9f728a90d119
famouscat <3142362556@qq.com>
2022-05-30 00:53:16 +0800

    update: 实现 Query.subcampus.university

--- src/subcampus/subcampus.service.ts
+++ src/subcampus/subcampus.service.ts
@@ -1,12 +1,34 @@
 import { Injectable } from '@nestjs/common'
 
 import { RelayPagingConfigArgs } from '../connections/models/connections.model'
+import { DbService } from '../db/db.service'
+import { University } from '../universities/models/universities.models'
 import { CreateSubCampusArgs } from './models/subcampus.model'
 
 @Injectable()
 export class SubcampusService {
+  constructor (private readonly dbService: DbService) {}
+
   async university (id: string) {
-    throw new Error('Method not implemented.')
+    const query = `
+        query v($id: string) {
+            var(func: uid($id)) @filter(type(SubCampus)) {
+                university as ~subcampus @filter(type(University))
+            }
+            university(func: uid(university)) {
+                id: uid
+                expand(_all_)
+            }
+        }
+      `
+    const res = await this.dbService.commitQuery<{
+      university: University[]
+    }>({
+      query,
+      vars: { $id: id }
+    })
+
+    return res.university[0]
   }
 
   async createSubCampus (args: CreateSubCampusArgs) {
