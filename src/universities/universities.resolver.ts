c21f4b57d3b44a390548f8777498c9d937c58001
famouscat <3142362556@qq.com>
2022-05-29 23:32:07 +0800

    feature: 声明 ResolveField.subjects, ResolveField.users, ResolveField.subcampuses, ResolveField.institutes

--- src/universities/universities.resolver.ts
+++ src/universities/universities.resolver.ts
@@ -1,8 +1,12 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql'
 
 import { NoAuth, Roles } from '../auth/decorator'
 import { Role } from '../auth/model/auth.model'
 import { RelayPagingConfigArgs } from '../connections/models/connections.model'
+import { InstitutesConnection } from '../institutes/models/institutes.model'
+import { SubCampusesConnection } from '../subcampus/models/subcampus.model'
+import { SubjectsConnection } from '../subject/model/subject.model'
+import { UsersConnection } from '../user/models/user.model'
 import { CreateUniversityArgs, UniversitiesConnection, University } from './models/universities.models'
 import { UniversitiesService } from './universities.service'
 
@@ -27,5 +31,25 @@ export class UniversitiesResolver {
   //   添加权限检测
   async createUniversity (@Args() args: CreateUniversityArgs) {
     return await this.universitiesService.createUniversity(args)
+  }
+
+  @ResolveField(of => InstitutesConnection, { description: '该大学的所有学院' })
+  async institutes (@Parent() university: University, @Args() args: RelayPagingConfigArgs) {
+    return await this.universitiesService.institutes(university.id, args)
+  }
+
+  @ResolveField(of => SubCampusesConnection, { description: '该大学的所有校区' })
+  async subcampuses (@Parent() university: University, @Args() args: RelayPagingConfigArgs) {
+    return await this.universitiesService.subcampuses(university.id, args)
+  }
+
+  @ResolveField(of => UsersConnection, { description: '该大学内的所有 User' })
+  async users (@Parent() university: University, @Args() args: RelayPagingConfigArgs) {
+    return await this.universitiesService.users(university.id, args)
+  }
+
+  @ResolveField(of => SubjectsConnection, { description: '该大学拥有的所有 Subject' })
+  async subjects (@Parent() university: University, @Args() args: RelayPagingConfigArgs) {
+    return await this.universitiesService.subjects(university.id, args)
   }
 }
--- src/universities/universities.service.ts
+++ src/universities/universities.service.ts
@@ -5,6 +5,22 @@ import { CreateUniversityArgs } from './models/universities.models'
 
 @Injectable()
 export class UniversitiesService {
+  async subjects (id: string, args: RelayPagingConfigArgs) {
+    throw new Error('Method not implemented.')
+  }
+
+  async users (id: string, args: RelayPagingConfigArgs) {
+    throw new Error('Method not implemented.')
+  }
+
+  async subcampuses (id: string, args: RelayPagingConfigArgs) {
+    throw new Error('Method not implemented.')
+  }
+
+  async institutes (id: string, args: RelayPagingConfigArgs) {
+    throw new Error('Method not implemented.')
+  }
+
   async createUniversity (args: CreateUniversityArgs) {
     throw new Error('Method not implemented.')
   }
