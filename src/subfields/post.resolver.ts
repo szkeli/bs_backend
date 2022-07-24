aa0758e240e7f3af60384fead3b6c4f7a9ac0bb3
famouscat <onismaa@gmail.com>
2022-07-24 23:40:08 +0800

    update: Post.subField

--- src/subfields/post.resolver.ts
+++ src/subfields/post.resolver.ts
@@ -1,10 +1,34 @@
-import { ResolveField, Resolver } from '@nestjs/graphql'
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql'
 
+import { DbService } from '../db/db.service'
 import { Post } from '../posts/models/post.model'
 import { SubField } from './models/subfields.model'
 
 @Resolver(of => Post)
 export class PostResolver {
-  @ResolveField(of => SubField, { description: '帖子所在的 SubField' })
-  async subField () {}
+  constructor (private readonly dbService: DbService) {}
+
+  @ResolveField(of => SubField, { nullable: true, description: '帖子所在的 SubField' })
+  async subField (@Parent() post: Post) {
+    const { id } = post
+    const query = `
+        query v($id: string) {
+            var(func: uid($id)) @filter(type(Post)) {
+                s as ~posts @filter(type(SubField))
+            }
+            subField(func: uid(s)) {
+                id: uid
+                expand(_all_)
+            }
+        }
+    `
+    const res = await this.dbService.commitQuery<{
+      subField: SubField[]
+    }>({
+      query,
+      vars: { $id: id }
+    })
+
+    return res.subField[0]
+  }
 }
--- src/subfields/subfields.module.ts
+++ src/subfields/subfields.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common'
 
 import { DbModule } from '../db/db.module'
 import { SharedModule } from '../shared/shared.module'
+import { PostResolver } from './post.resolver'
 import { SubfieldsResolver } from './subfields.resolver'
 import { SubfieldsService } from './subfields.service'
 import { SubjectResolver } from './subject.resolver'
 
 @Module({
   imports: [SharedModule, DbModule],
-  providers: [SubfieldsResolver, SubfieldsService, SubjectResolver]
+  providers: [SubfieldsResolver, SubfieldsService, SubjectResolver, PostResolver]
 })
 export class SubfieldsModule {}
